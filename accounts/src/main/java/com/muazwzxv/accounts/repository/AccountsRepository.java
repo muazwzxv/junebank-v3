5d91c208f8f3e58110c85e26f5375beabb721615
Muash <muazwazir@gmail.com>
2025-05-13 01:09:58 +0800

    feat: enhance user defined query to wrap in transactions

--- accounts/src/main/java/com/muazwzxv/accounts/repository/AccountsRepository.java
+++ accounts/src/main/java/com/muazwzxv/accounts/repository/AccountsRepository.java
@@ -1,7 +1,9 @@
 package com.muazwzxv.accounts.repository;
 
 import com.muazwzxv.accounts.entities.Accounts;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,5 +12,7 @@ import java.util.Optional;
 public interface AccountsRepository extends JpaRepository<Accounts, Long> {
     Optional<Accounts> findByCustomerId(Long customerId);
 
+    @Transactional
+    @Modifying
     Optional<Accounts> findByAccountNumber(Long accountNumber);
 }
