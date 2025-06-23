82a00a2df095b10d06dfd8c9c7d7162e931caa40
Muash <muazwazir@gmail.com>
2025-06-24 01:50:02 +0800

    feat: schema for cards

---
+++ card-service/src/main/resources/schema.sql
@@ -0,0 +1,52 @@
+CREATE TABLE IF NOT EXISTS `order` (
+    `id` int AUTO_INCREMENT PRIMARY KEY,
+
+    `customer_uuid` varchar(36),
+    `design_uuid` varchar,
+    `status` varchar,
+
+    `created_at` date NOT NULL,
+    `created_by` varchar(20) NOT NULL,
+    `updated_at` date DEFAULT NULL,
+    `updated_by` varchar(20) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `design` (
+    `id` int AUTO_INCREMENT PRIMARY KEY,
+
+    `design_uuid` varchar(36),
+    `description` varchar,
+    `status` varchar(30),
+
+    `created_at` date NOT NULL,
+    `created_by` varchar(20) NOT NULL,
+    `updated_at` date DEFAULT NULL,
+    `updated_by` varchar(20) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `transaction` (
+    `id` int AUTO_INCREMENT PRIMARY KEY,
+
+    `transaction_uuid` varchar(36),
+    `card_uuid` varchar(36),
+    `type` varchar, -- charge, refund
+    `status` varchar(30),
+
+    `created_at` date NOT NULL,
+    `created_by` varchar(20) NOT NULL,
+    `updated_at` date DEFAULT NULL,
+    `updated_by` varchar(20) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `card` (
+    `id` int AUTO_INCREMENT PRIMARY KEY,
+
+    `card_uuid` varchar(36),
+    `customer_uuid` varchar(36),
+    `status` varchar,
+
+    `created_at` date NOT NULL,
+    `created_by` varchar(20) NOT NULL,
+    `updated_at` date DEFAULT NULL,
+    `updated_by` varchar(20) DEFAULT NULL
+);
